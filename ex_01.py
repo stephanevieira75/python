1b5926c442ad8cddb8e2d64fe2564145e553ec3d
Vieira <stephane.vieira75@outlook.fr>
2018-04-20 22:11:10 +0200

    second push ex_01.py

--- ex_01.py
+++ ex_01.py
@@ -1,23 +1,17 @@
-
-# Quote
+# Quotes
 quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau etre ou ne pas etre, nous sommes !" , "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
-user_answer = input('Tapez entree pour connaitre une autre citation ou B pour quitter le programme')
-# If user_answer == "B":
-if user_answer == "B":
-    # - Leave the program
-    print('Bye bye !') + exit()
-# Elif
-elif user_answer == "C":
-    print("C pas la bonne reponse, Et G pas d'\humour, je C ...")
-# Else: 
-else:
-    pass
-    # - Show another quote
 
+characters = ["alvin et les Chipmunks", "babar", "betty boop", "calimero", "casper", "le chat potte"]
 def show_random_quote(my_list):
     # - Get a random number
     item = my_list[0] # Get a quote from a list
-    print(item) # Show the quote in the interpreter
-    return "Program is over" # Returned value
+    return item # Returned value
 
-print(show_random_quote(quotes))
+user_answer = input('Tapez entree pour connaitre une autre citation ou B pour quitter le programme')
+while user_answer != "B":
+    print(show_random_quote(quotes))
+    user_answer = input('Tapez entree pour connaitre une autre citation ou B pour quitter le programme')
+
+for character in characters:
+    n_character = character.capitalize()
+    print(n_character)
