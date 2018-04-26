5097019411e2629cc6fac593559dd4291ed56350
Vieira <stephane.vieira75@outlook.fr>
2018-04-26 19:51:29 +0200

    modify parite.py and analasys/xml.py

--- python_poo_2/ex_02/parite.py
+++ python_poo_2/ex_02/parite.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python3
 # coding: utf-8
+import argparse
 
 import csv_analysis as c_an
 import xml_analysis as x_an
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--extension", help="""Type of file to analyse. Is it a CSV of an XML?""")
+    return parser.parse_args()
+    
 def main():
     c_an.launch_analysis('current_mps.csv')
     x_an.launch_analysis('SyceronBrut.xml')
