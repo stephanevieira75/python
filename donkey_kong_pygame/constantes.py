eaf3f81197b9a63e64df4d87b109e9fc4ebdb3b8
stephane <stephane.vieira75@outlook.fr>
2018-09-25 23:43:02 +0200

    Donkey-Kong V1

---
+++ donkey_kong_pygame/constantes.py
@@ -0,0 +1,17 @@
+"""Constantes du jeu de Labyrinthe Donkey Kong"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "Donkey Kong - Labyrinthe"
+image_icone = "img/dk_droite.png"
+
+#Listes des images du jeu
+image_accueil = "img/accueil.png"
+image_fond = "img/fond.jpg"
+image_mur = "img/mur.png"
+image_depart = "img/depart.png"
+image_arrivee = "img/arrivee.png"
